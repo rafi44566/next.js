ff0a09dfdf289e4357c9698de3ffacb7273c3448
Tobias Koppers <tobias.koppers@googlemail.com>
2026-01-09 15:11:07 +0100

    avoid pub fields for SingleModuleGraphWithBindingUsage

--- turbopack/crates/turbopack-core/src/module_graph/mod.rs
+++ turbopack/crates/turbopack-core/src/module_graph/mod.rs
@@ -910,12 +910,12 @@ impl ModuleGraph {
 }
 
 #[derive(
-    Clone, Debug, PartialEq, Eq, Hash, TaskInput, TraceRawVcs, NonLocalValue, Encode, Decode,
+    Clone, Copy, Debug, PartialEq, Eq, Hash, TaskInput, TraceRawVcs, NonLocalValue, Encode, Decode,
 )]
 pub struct SingleModuleGraphWithBindingUsage {
-    pub graph: OperationVc<SingleModuleGraph>,
-    pub graph_idx: u32,
-    pub binding_usage: Option<OperationVc<BindingUsageInfo>>,
+    graph: OperationVc<SingleModuleGraph>,
+    graph_idx: u32,
+    binding_usage: Option<OperationVc<BindingUsageInfo>>,
 }
 
 impl SingleModuleGraphWithBindingUsage {
@@ -1008,6 +1008,10 @@ impl ModuleGraphRef {
         &self,
     ) -> impl Iterator<Item = (NodeIndex, &'_ SingleModuleGraphNode)> + '_ {
         self.graphs.iter().flat_map(|g| g.enumerate_nodes())
+    }
+
+    pub fn iter_nodes(&self) -> impl Iterator<Item = ResolvedVc<Box<dyn Module>>> + '_ {
+        self.graphs.iter().flat_map(|g| g.iter_nodes())
     }
 
     /// Iterate the edges of a node REVERSED!
